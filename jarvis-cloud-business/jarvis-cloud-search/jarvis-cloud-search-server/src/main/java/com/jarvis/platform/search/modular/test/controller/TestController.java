3dcfb1fb2e2252cbfc41fc46fae04d4d1332d8e5
Doug Wang <wangtao@deehero.com>
2024-04-12 13:41:12 +0800

    feat: openfeign测试

---
+++ jarvis-cloud-business/jarvis-cloud-search/jarvis-cloud-search-server/src/main/java/com/jarvis/platform/search/modular/test/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.jarvis.platform.search.modular.test.controller;
+
+import com.jarvis.framework.openfeign.annotation.PermitRequest;
+import com.jarvis.platform.auth.modular.log.entity.SystemLog;
+import com.jarvis.platform.client.auth.feign.RemoteLogClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Doug Wang
+ * @since 1.8, 2024-04-12 13:16:45
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private RemoteLogClient remoteLogClient;
+
+    @PermitRequest
+    @GetMapping("/test")
+    public String test() {
+        remoteLogClient.create(new SystemLog());
+        return "ok!";
+    }
+
+}
