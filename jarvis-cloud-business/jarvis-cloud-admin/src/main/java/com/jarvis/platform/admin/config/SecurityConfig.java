ca9cbd9d209a5a5125befc8713f03d3c3c839fdb
Doug Wang <wangtao@deehero.com>
2024-04-17 14:38:03 +0800

    feat: first commit

--- jarvis-cloud-business/jarvis-cloud-admin/src/main/java/com/jarvis/platform/admin/config/SecurityConfig.java
+++ jarvis-cloud-business/jarvis-cloud-admin/src/main/java/com/jarvis/platform/admin/config/SecurityConfig.java
@@ -8,13 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+/**
+ * @author Doug Wang
+ * @since 1.8, 2024-04-17 14:24:08
+ */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private String adminContextPath;
+    private final String adminContextPath;
+
     public SecurityConfig(AdminServerProperties adminServerProperties) {
         this.adminContextPath = adminServerProperties.getContextPath();
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
